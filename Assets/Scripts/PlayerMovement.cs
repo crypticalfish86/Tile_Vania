ec0e29d3feb6033755ae0e9accd677fe15b12906
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-08-02 01:15:18 +0100

    run animation implemented on player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements.Experimental;
 
 public class PlayerMovement : MonoBehaviour
 {
     
     private Vector2 moveInput; //user input to move player
     private Rigidbody2D myRigidBody; //player physics
+    private Animator playerAnimator;//the animator of the player
 
     [SerializeField] float runSpeed = 8f;
 
@@ -15,6 +18,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,19 +33,30 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log("Jumped");
     }
 
+    //changes the value of moveInput
     private void OnMove(InputValue value){
         moveInput = value.Get<Vector2>();
     }
+        //adds velocity in the x direction to our player object
         private void Run(){
             Vector2 playerVelocity = new Vector2 (runSpeed * moveInput.x, myRigidBody.velocity.y); //keeps velocity the same (so if you have gravity on it stays the same)
             myRigidBody.velocity = playerVelocity; //rigidbody.velocity is the speed in a certain direction of the physics of your object
+
+            //if player absolute velocity value is greater than near 0, set running animation, otherwise unset it
+            bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon; //if absolute velocity value is greater than near 0 value(Mathf.Epsilon)
+            if(playerHasHorizontalSpeed){
+                playerAnimator.SetBool("isRunning", true);
+            }
+            else{
+                playerAnimator.SetBool("isRunning", false);
+            }
         }
-        
+        //flips player sprite when moving right or left
         private void FlipSprite(){
-            bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+            bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon; //if absolute velocity value is greater than near 0 value(Mathf.Epsilon)
 
             if (playerHasHorizontalSpeed){
-                transform.localScale = new Vector2 (Mathf.Sign(myRigidBody.velocity.x), 1f);
+                transform.localScale = new Vector2 (Mathf.Sign(myRigidBody.velocity.x), 1f); //sprite scale is either -1 or 1 due to Mathf.Sign which just determines +ve/-ve value
             }
         }
 }

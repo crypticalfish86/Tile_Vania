eb4a8ef64797a0699925c9d263842721adc46b5e
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-08-07 23:29:56 +0100

    projectile mechanic added (able to shoot and kill enemy)

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody2D myRigidBody;
+    [SerializeField] float bulletSpeed;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        
+        Vector2 playerScale = FindFirstObjectByType<PlayerInput>().transform.localScale;
+        transform.localScale = playerScale;
+        float bulletVelocity = bulletSpeed * playerScale.x; //multiply speed by x scale to shoot bullet right way
+        myRigidBody.velocity = new Vector2 (bulletVelocity, 0);
+    }
+
+    //Destroy itself on impact with anything, If bullet hits enemy destroy the enemy too
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Enemy"){
+            Destroy(other.gameObject);
+        }
+        Destroy(gameObject);
+    }
+}
